dd2467c47dc48678d7307c9da9cec6bc464b4c67
MDTufan <129486543+MDTufan@users.noreply.github.com>
2023-06-21 05:46:33 -0700

    Add files via upload
    This is Const section

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+
+// alert("Hello");
+
+
+// const MyName="Tufan Ali";
+// console.log(MyName);
+
+// function InchesToFeet(inche){
+//     let Feet=inche/12;
+//     return Feet;
+// }
+// let Result=InchesToFeet(200);
+// console.log(Result);
+
+
+// function Kilumiles(miles){
+//     let FastRselt=miles*1.60934;
+//     return  FastRselt;
+// }
+// let Result=Kilumiles(30);
+// console.log(Result);
+
+
+//getting firstname and lastname from user
+// var firstName = prompt("Enter the first Name : ")
+// var lastName = prompt("Enter Your Last name : ")
+
+//adding firstname and lastname
+// var fullName = firstName + lastName;
+
+//printing the full name
+// document.write("Fullname is : " + fullName + "<br/>")
+
+//printing the length of full name
+// document.write("Length of full name : " + fullName.length + "<br/>")
+
+//printing the full name in uppercase
+// document.write("Uppercase Full name : " + fullName.toUpperCase())
+
+// var sum = 0
+// var i = 1
+// while (i <= 100) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         sum = sum + i
+//     }
+//     i = i + 1
+// }
+// document.write("sum is : " + sum)
